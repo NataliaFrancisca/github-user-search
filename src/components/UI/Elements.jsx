1c695d5c41d86d727f81ad01e468b796a270f90a
NataliaFrancisca <nathifrancisca@gmail.com>
2022-07-11 14:44:32 -0300

    feat: using conditional to handle with error

--- src/components/UI/Elements.jsx
+++ src/components/UI/Elements.jsx
@@ -1,16 +1,19 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export const FlexGroup = ({icon, title}) => {
     return(
         <span className="font-cardFont flex items-center">
-            <label class="material-symbols-outlined">{icon}</label>
-            <p className={`ml-2 font-normal text-sm`}>{title}</p>
+            {title 
+                ? [<FontAwesomeIcon icon={icon} />, <p className={`ml-2 font-normal text-sm`}>{title}</p>]
+                : [<FontAwesomeIcon icon={icon} className="text-slate-400"/>, <p className={`ml-2 font-normal text-sm text-slate-400`}>Not Available</p>]
+            }
         </span>
     )
 }
 
-
 export const FlexColumnGroup = ({title, text}) => {
     return(
-        <span className="pr-20 font-cardFont">
+        <span className="font-cardFont">
             <label className={`text-sm`}>{title}</label>
             <p className="mt-1 font-semibold text-lg">{text}</p>
         </span>
