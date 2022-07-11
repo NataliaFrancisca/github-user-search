c163722b0afd0e6ee625bf1ce51455748dfeb9b4
NataliaFrancisca <nathifrancisca@gmail.com>
2022-07-11 14:45:20 -0300

    feat: change google icon for fontawesome

--- src/components/ButtonTheme/index.jsx
+++ src/components/ButtonTheme/index.jsx
@@ -1,6 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
-import {getTheme, onChangeTheme} from "../../functions/ChangeTheme";
+import { getTheme, onChangeTheme } from "../../functions/change_theme";
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 
 const ButtonTheme = () => {
 
@@ -13,9 +16,8 @@ const ButtonTheme = () => {
             <span className={"font-menuFont text-xl font-normal pr-2"}>{currentTheme == 'light' ? "dark" : 'light'}</span>
             <button onClick={() => onChangeTheme()}>
                 {currentTheme == 'light' 
-                    ? <span className="material-symbols-outlined font-semibold">dark_mode</span> 
-                    : <span className="material-symbols-outlined font-semibold">light_mode</span> 
-                  
+                    ? <FontAwesomeIcon icon={faMoon} />
+                    : <FontAwesomeIcon icon={faSun} />
                 }
             </button>
         </div>
