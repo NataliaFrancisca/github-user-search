a728a3b7b03d1942b4fe6320daead211466514a6
NataliaFrancisca <nathifrancisca@gmail.com>
2022-07-11 14:45:53 -0300

    feat: import fontawesome

--- src/components/CardDev/index.jsx
+++ src/components/CardDev/index.jsx
@@ -1,41 +1,51 @@
 import { FlexGroup, FlexColumnGroup } from "../UI/Elements";
 
+import { formatDate } from "../../functions/format_date";
+
+import { faLocationDot, faLink, faHouseLaptop } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+
 const CardDev = ({backgroundCard, backgroundBody, cardColor, dataUser}) => {
 
-    const {avatar_url, name, html_url, login, bio, company, location, public_repos, following, followers, twitter_username} = dataUser;
+    const { followers, following } = dataUser;
+    const { avatar_url, name, login, location } = dataUser;
+    const { html_url, bio, company, public_repos, twitter_username, created_at} = dataUser;
 
     return(
-        <article className={`w-full flex justify-between mt-6 p-10 rounded-lg space-x-6 ${backgroundCard}`}>
-             <img className="rounded-full object-cover h-24 w-24 md:h-32 md:w-32" 
-                src={avatar_url} />
+        <article className={`w-full flex flex-col mt-6 p-8 rounded-lg items-end ${backgroundCard}`}>
 
-            <section className="w-5/6 font-cardFont">
+            <section className="w-full flex justify-between">
+                <img className="rounded-full object-cover h-24 w-24" src={avatar_url} />
 
-                <aside className="flex align-top justify-between w-full flex-col md:flex-row" > 
+                <aside className="flex w-9/12 ml-4 align-top justify-between flex-col md:flex-row " > 
                     <div>
-                        <h1 className="font-cardFont text-xl font-semibold">{name}</h1>
-                        <span className="mt-2 text-blue-400">{`@${login}`}</span>
+                        <h1 className="font-cardFont text-xl font-semibold mb-2">{name}</h1>
+                        <a href={html_url} className="text-blue-400">{`@${login}`}</a>
                     </div>
 
-                    <span className="font-cardFont text-sm font-normal"> Joined 25 Jan 2020</span>
+                    <span className="font-cardFont text-sm font-normal mt-2 lg:mt-0">{`Joined ${formatDate(created_at)}`}</span>
                 </aside>
-
-                <p className={`my-4 text-base ${cardColor}`}>{bio}</p>
-
-                <aside className={`flex p-6 rounded-lg ${backgroundBody} ${cardColor}`}>
-                    <FlexColumnGroup title="Repos" text={public_repos} />
-                    <FlexColumnGroup title="Followers" text={followers} />
-                    <FlexColumnGroup title="Following" text={following} />
-                </aside>
-
-                <aside className={`grid grid-cols-2 gap-2 mt-4 ${cardColor}`}>
-                    <FlexGroup icon="where_to_vote" title={location} />
-                    <FlexGroup icon="public" title={twitter_username ? twitter_username : "Not Available"}/>
-                    <FlexGroup icon="link" title='nat.com.br' />
-                    <FlexGroup icon="home_work" title={company} />
-                </aside>
-
             </section>
+
+            <section className="w-full flex justify-end mt-6 md:mt-0">
+                <div className="flex flex-col w-full md:w-9/12" > 
+                    <p className={`text-base ${cardColor}`}>{bio ? bio : "Not Available"} </p>
+
+                    <aside className={`flex p-6 rounded-lg my-2 space-x-4 md:space-x-8 ${backgroundBody} ${cardColor}`}>
+                        <FlexColumnGroup title="Repos" text={public_repos} />
+                        <FlexColumnGroup title="Followers" text={followers} />
+                        <FlexColumnGroup title="Following" text={following} />
+                    </aside>
+
+                    <aside className={`grid grid-cols-1 gap-2 my-2 sm:grid-cols-2 ${cardColor}`}>
+                        <FlexGroup icon={faLocationDot} title={location} />
+                        <FlexGroup icon={faTwitter} title={twitter_username} />
+                        <FlexGroup icon={faLink} title={'nat@mail.com'} />
+                        <FlexGroup icon={faHouseLaptop} title={company} />
+                    </aside>
+                </div>
+            </section>
+
         </article>
     )
 }
