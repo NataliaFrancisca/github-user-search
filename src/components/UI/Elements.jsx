9ce4cbe82aaef45be79c259ca7616c0022712714
NataliaFrancisca <nathifrancisca@gmail.com>
2022-07-12 10:12:40 -0300

    feat: add component message error and change flex group

--- src/components/UI/Elements.jsx
+++ src/components/UI/Elements.jsx
@@ -2,12 +2,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export const FlexGroup = ({icon, title}) => {
     return(
-        <span className="font-cardFont flex items-center">
+        <li className="font-cardFont flex items-center">
             {title 
-                ? [<FontAwesomeIcon icon={icon} />, <p className={`ml-2 font-normal text-sm`}>{title}</p>]
-                : [<FontAwesomeIcon icon={icon} className="text-slate-400"/>, <p className={`ml-2 font-normal text-sm text-slate-400`}>Not Available</p>]
+                ? [<FontAwesomeIcon icon={icon} fixedWidth size="lg" />, <p className={`ml-2 font-normal text-sm`}>{title}</p>]
+                : [<FontAwesomeIcon icon={icon} fixedWidth size="lg" className="text-slate-400"/>, <p className={`ml-2 font-normal text-sm text-slate-400`}>Not Available</p>]
             }
-        </span>
+        </li>
     )
 }
 
@@ -17,5 +17,14 @@ export const FlexColumnGroup = ({title, text}) => {
             <label className={`text-sm`}>{title}</label>
             <p className="mt-1 font-semibold text-lg">{text}</p>
         </span>
+    )
+}
+
+export const MessageError = ({backgroundCard, statusCode}) => {
+    return(
+        <div className={`w-full flex flex-col mt-6 p-8 rounded-lg center ${backgroundCard}`}>
+            <h1 className="text-6xl font-menuFont font-semibold">{statusCode}</h1>
+            <span className="mt-4 text-lg font-menuFont font-semibold tracking-wide">something wrong, try another user!!!</span>
+        </div>
     )
 }
