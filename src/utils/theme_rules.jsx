d4c0460a6a5b4d7b88e88c76edbd20a752e178f0
NataliaFrancisca <nathifrancisca@gmail.com>
2022-07-12 11:03:57 -0300

    feat: create theme_rules file

---
+++ src/utils/theme_rules.jsx
@@ -0,0 +1,11 @@
+export const lightTheme = {
+    body: "bg-bg_light text-black",
+    card: "bg-card_light text-black",
+    cardColor: "text-black" 
+};
+
+export const darkTheme = { 
+    body: "bg-bg_dark text-white",
+    card: 'bg-card_dark text-white',
+    cardColor: "text-gray-300"
+};
